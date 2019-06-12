794331d62f4385631ed4ca84b4d336441248d33f
MatthieuCunha <33487642+MatthieuCunha@users.noreply.github.com>
2019-06-12 08:44:56 +0200

    exe

---
+++ ui_client.h
@@ -0,0 +1,49 @@
+/********************************************************************************
+** Form generated from reading UI file 'client.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_CLIENT_H
+#define UI_CLIENT_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Client
+{
+public:
+
+    void setupUi(QDialog *Client)
+    {
+        if (Client->objectName().isEmpty())
+            Client->setObjectName(QStringLiteral("Client"));
+        Client->resize(400, 300);
+
+        retranslateUi(Client);
+
+        QMetaObject::connectSlotsByName(Client);
+    } // setupUi
+
+    void retranslateUi(QDialog *Client)
+    {
+        Client->setWindowTitle(QApplication::translate("Client", "Client", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Client: public Ui_Client {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_CLIENT_H
